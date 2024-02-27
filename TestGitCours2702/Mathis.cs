121dde720118f7fa38f31510b60ee2934ecb3978
BonhoureMathis <153090560+BonhoureMathis@users.noreply.github.com>
2024-02-27 11:48:56 +0100

    MethodeMathis

---
+++ TestGitCours2702/Mathis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitCours2702
+{
+    internal class Mathis
+    {
+        public void MaMethode()
+        {
+            Console.WriteLine("Ceci est la methode de Mathis");
+        }
+    }
+}
