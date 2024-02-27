d58fa56d01886a40682d14f2585393758e9c94f5
GuilhemBdt <157315098+GuilhemBdt@users.noreply.github.com>
2024-02-27 11:50:10 +0100

    Methode Guilhem
    Création de la méthode de Guilhem

---
+++ TestGitCours2702/Guilhem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGitCours2702
+{
+    internal class Guilhem
+    {
+        public void MaMethode()
+        {
+            Console.WriteLine("Ceci est la methode dde Guilhem");
+        }
+    }
+}
